de4c8a63377c210adf9dd7cd1237346edc44e927
Jake <findjakebian@gmail.com>
2015-06-10 19:32:23 -0700

    dev: better grunt configs, copy react to client folder

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,21 +10,48 @@ module.exports = function (grunt) {
                 sourceMap: true
             },
             dist: {
-                files: {
-                   "./server/build/server.js": ["./server/src/server.js"]
-                }
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'client/js/src',
+                        src: ['**/*.js'],
+                        dest: 'client/js/build/',
+                        ext:'.js'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'server/src',
+                        src: ['**/*.js'],
+                        dest: 'server/build/',
+                        ext:'.js'
+                    }
+                ]
             }
 
         },
 
-      watch: {
-        scripts: {
-            files: ['**/src/**/*.js'],
-            tasks: ['babel'],
+        watch: {
+            scripts: {
+                files: ['**/src/**/*.js'],
+                tasks: ['babel'],
+            },
         },
-      },
+
+        copy: {
+            main: {
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: 'node_modules/react/dist/*.js',
+                        dest: 'client/deps/react/'
+                    }
+                ]
+            }
+        }
     });
 
+    grunt.registerTask('loadDeps', ['copy'])
     grunt.registerTask('default', ['babel']);
 
 }
